2b54b93f1b9722b2f4141ea1370254025c6f70eb
Ash Blue <ash@blueashes.com>
2015-06-28 00:39:18 -0700

    Initial commit

---
+++ Assets/Standard Assets/2D/Scripts/Restarter.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets._2D
+{
+    public class Restarter : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.tag == "Player")
+            {
+                Application.LoadLevel(Application.loadedLevelName);
+            }
+        }
+    }
+}
