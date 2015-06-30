83d7d39be3f64f52959197fd1c55835bbdf80015
Ash Blue <ash@blueashes.com>
2015-06-29 18:23:20 -0700

    Added support for repeating parallax elements

--- Assets/Parallax2D/Scripts/Parallax2D.cs
+++ Assets/Parallax2D/Scripts/Parallax2D.cs
@@ -3,12 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 
 namespace Adnc.Parallax {
+	/// <summary>
+	/// Note that you need to adjust the script execution order and place this element higher than the ParallaxLayer element
+	/// </summary>
 	public class Parallax2D : MonoBehaviour {
+		public static Parallax2D current;
 		[SerializeField] bool debug;
 
 		[Tooltip("Tag to auto parallax elements. Only runs at startup.")]
 		[TagAttribute, SerializeField] public string autoParallaxTag;
 
+		// @TODO Consider ditching this
 		[Tooltip("Tag to auto parallax an element that repeats (must be in a container). Only runs at startup.")]
 		[TagAttribute, SerializeField] public string autoParallaxRepeatTag;
 
@@ -34,81 +39,109 @@ namespace Adnc.Parallax {
 		[Tooltip("Set the maximum layer speed of the foreground elements. 1 is the speed of the target, 0 would be no movement.")]
 		[SerializeField, Range(0f, 1f)] float foregroundMaxSpeed = 0.2f;
 
-
 		float maxZDistance = 0f; // Furthest away element
 		float minZDistance = 0f; // Nearest element
 		bool loop = true; // Is parallax currently active?
 		Vector3 prevPos; // Location of the camera last frame
-		public static List<ParallaxLayer> parallaxLayers = new List<ParallaxLayer>(); // Record of all current parallax layers
+		public ScreenRect screen = new ScreenRect();
+		public List<ParallaxLayer> parallaxLayers = new List<ParallaxLayer>(); // Record of all current parallax layers
 
 		void Awake () {
 			if (cam == null) cam = Camera.main;
+
+			if (current != null) {
+				Debug.LogError("Only 1x Parallax2D script may be active at a time. Delete the current Parallax2D script before creating a new one. Aborting.");
+				Destroy(gameObject);
+				return;
+			}
+
+			current = this;
 		}
 
 		void Start () {
-			Restart();
-
+			Play();
 			// @TODO We should try to auto calculate a default speed based on max distances, need to have a fallback that can just parallax with one element
 		}
 
-		public void Restart () {
+		/// <summary>
+		/// Clear out all elements we are parallaxing
+		/// </summary>
+		public void Reset () {
 			parallaxLayers.Clear();
+		}
 
+		public void Play () {
 			foreach (GameObject go in GameObject.FindGameObjectsWithTag(autoParallaxTag)) {
 				go.AddComponent(typeof(ParallaxLayer));
 			}
-
+			
 			foreach (GameObject go in GameObject.FindGameObjectsWithTag(autoParallaxRepeatTag)) {
 				go.AddComponent(typeof(ParallaxLayer));
 			}
-
+			
 			// Loop through and discover all relative parallax distances
 			foreach (ParallaxLayer layer in parallaxLayers) {
 				if (layer.transform.position.z > maxZDistance) maxZDistance = layer.transform.position.z;
 				if (layer.transform.position.z < minZDistance) minZDistance = layer.transform.position.z;
 			}
-
+			
 			// Override discoverd distances if defaults have been provided
 			if (defaultMaxZDistance != 0f) maxZDistance = defaultMaxZDistance;
 			if (defaultMinZDistance != 0f) minZDistance = defaultMinZDistance;
-
-			if (debug) Debug.LogFormat("Max Min: {0}, Max: {1}", minZDistance, maxZDistance);
 			
+			if (debug) Debug.LogFormat("Max Min: {0}, Max: {1}", minZDistance, maxZDistance);
+
 			StopAllCoroutines();
 			StartCoroutine(FollowLoop());
 		}
 
 		IEnumerator FollowLoop () {
 			Vector3 targetSpeed;
-			float speed;
-			float speedX;
-			float speedY;
+			Vector2 speed;
+			float speedFactor;
 			Vector3 pos;
 
 			loop = true;
 			prevPos = cam.transform.position;
 
+			// Run setup after updating the camera boundary
+			screen.UpdateBoundary(cam);
+			foreach (ParallaxLayer layer in parallaxLayers) {
+				layer.ParallaxSetup();
+			}
+
 			while (loop) {
+
+				// Update our camera boundary records
+				screen.UpdateBoundary(cam);
+				
+				if (debug) {
+					ScreenRect.DrawBoundary(screen.rect, Color.green);
+				}
+
+				// Set proper speed for all elements
 				targetSpeed = cam.transform.position - prevPos;
 
 				foreach (ParallaxLayer layer in parallaxLayers) {
 					if (layer.transform.position.z > 0f) {
 						// Background element
-						speed = Mathf.Lerp(0f, backgroundMaxSpeed, layer.transform.position.z / maxZDistance);
+						speedFactor = Mathf.Lerp(0f, backgroundMaxSpeed, layer.transform.position.z / maxZDistance);
 					} else {
 						// Foreground element
-						speed = Mathf.Lerp(0f, foregroundMaxSpeed, layer.transform.position.z / minZDistance) * -1f;
+						speedFactor = Mathf.Lerp(0f, foregroundMaxSpeed, layer.transform.position.z / minZDistance) * -1f;
 					}
 
-					speedX = targetSpeed.x * speed * parallaxSpeedFactor.x;
-					speedY = targetSpeed.y * speed * parallaxSpeedFactor.y;
+					speed.x = targetSpeed.x * speedFactor * parallaxSpeedFactor.x;
+					speed.y = targetSpeed.y * speedFactor * parallaxSpeedFactor.y;
 
 					pos = layer.transform.position;
-					pos.x += speedX;
-					pos.y += speedY;
+					pos.x += speed.x;
+					pos.y += speed.y;
 					layer.transform.position = pos;
+
+					layer.ParallaxUpdate(speed);
 				}
-				
+
 				prevPos = cam.transform.position;
 
 				yield return null;
@@ -127,7 +160,7 @@ namespace Adnc.Parallax {
 		}
 
 		void OnDestroy () {
-			parallaxLayers.Clear();
+			current = null;
 		}
 	}
 }
--- Assets/Parallax2D/Scripts/ParallaxLayer.cs
+++ Assets/Parallax2D/Scripts/ParallaxLayer.cs
@@ -3,11 +3,98 @@ using System.Collections;
 
 namespace Adnc.Parallax {
 	public class ParallaxLayer : MonoBehaviour {
-		public Vector3 originPos; // Captured original position in-case we want to reset it
+		public bool debug;
+
+		[HideInInspector] public Vector3 originPos; // Captured original position in-case we want to reset it
+		static float repeatPadding = 1f; // How many units to spawn a repeated tile in advance
+
+		[SerializeField] bool repeat;
+		SpriteRenderer repeatSprite;
+
+		Rect rect = new Rect(); // Used to monitor the boundary of repeating parallax elements
 
 		void Awake () {
 			originPos = transform.position;
-			Parallax2D.parallaxLayers.Add(this);
+			Parallax2D.current.parallaxLayers.Add(this);
+		}
+
+		public void ParallaxSetup () {
+			if (repeat) {
+				repeatSprite = GetComponentInChildren<SpriteRenderer>();
+				if (repeatSprite.gameObject == gameObject) repeatSprite = null; // Ignore parent element
+				
+				if (repeatSprite == null) {
+					Debug.LogError("ParallaxLayer was marked as repeat, but no child element with SpriteRenderer was found to repeat. Disabling repeat.");
+					repeat = false;
+				} else {
+					rect.width = repeatSprite.bounds.size.x;
+					rect.height = repeatSprite.bounds.size.y;
+					rect.center = repeatSprite.bounds.center;
+					
+					// Make sure the repeat element repeats up until it shows on the current viewing window
+					while (IsNewRightBuddy()) {
+						AddRightBuddy(repeatSprite);
+					}
+
+					while (IsNewLeftBuddy()) {
+						AddLeftBuddy(repeatSprite);
+					}
+				}
+			}
+		}
+
+		bool IsNewRightBuddy () {
+			return Parallax2D.current.screen.rect.xMax + repeatPadding > rect.xMax;
+		}
+
+		bool IsNewLeftBuddy () {
+			return Parallax2D.current.screen.rect.xMin - repeatPadding < rect.xMin;
+		}
+
+		Vector2 rectCenter;
+		public void ParallaxUpdate (Vector2 change) {
+			if (repeat) {
+				// Reposition the rectangle to wrap the elements correctly
+				rect.position += change;
+
+				if (IsNewRightBuddy()) {
+					AddRightBuddy(repeatSprite);
+				}
+				
+				if (IsNewLeftBuddy()) {
+					AddLeftBuddy(repeatSprite);
+				}
+
+				if (debug) {
+					ScreenRect.DrawBoundary(rect, Color.gray);
+				}
+			}
+		}
+
+		public void AddRightBuddy (SpriteRenderer sprite) {
+			GameObject go = Instantiate(sprite.gameObject) as GameObject;
+			go.transform.SetParent(transform);
+
+			// Set position
+			Vector3 pos = rect.center;
+			pos.x = rect.xMax + sprite.bounds.extents.x;
+			go.transform.position = pos;
+
+			// Update wrapping rectangle
+			rect.xMax += sprite.bounds.size.x;
+		}
+
+		public void AddLeftBuddy (SpriteRenderer sprite) {
+			GameObject go = Instantiate(sprite.gameObject) as GameObject;
+			go.transform.SetParent(transform);
+			
+			// Set position
+			Vector3 pos = rect.center;
+			pos.x = rect.xMin - sprite.bounds.extents.x;
+			go.transform.position = pos;
+			
+			// Update wrapping rectangle
+			rect.xMin -= sprite.bounds.size.x;
 		}
 	}
 }
