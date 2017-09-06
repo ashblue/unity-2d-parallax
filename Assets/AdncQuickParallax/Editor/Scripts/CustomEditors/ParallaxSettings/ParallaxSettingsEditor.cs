1aa0f54e90a7b484682c865af2bfbc646812aaab
Ash Blue <ash@blueashes.com>
2017-09-06 00:37:59 -0600

    Added Vector2 variables to settings

---
+++ Assets/AdncQuickParallax/Editor/Scripts/CustomEditors/ParallaxSettings/ParallaxSettingsEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Adnc.QuickParallax.Editors.CustomEditors {
+	[CustomEditor(typeof(ParallaxSettings))]
+	public class ParallaxSettingsEditor : Editor {
+		private ParallaxSettingsListVector2Variable _vector2Variables;
+
+		private void OnEnable () {
+			_vector2Variables = new ParallaxSettingsListVector2Variable(this, "_vector2Variables", "Vector2 Variables");
+		}
+
+		public override void OnInspectorGUI () {
+			base.OnInspectorGUI();
+
+			EditorGUILayout.HelpBox("These Vector2 variables can be customized and re-used", MessageType.Info);
+			_vector2Variables.Update();
+		}
+	}
+}
+
---
+++ Assets/AdncQuickParallax/Scripts/Settings/ParallaxSettings.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Adnc.QuickParallax {
+	[CreateAssetMenu(fileName = "ParallaxSettings", menuName = "ADNC/Parallax/Settings", order = 1)]
+	public class ParallaxSettings : ScriptableObject {
+		private static ParallaxSettings _current;
+		private const string RESOURCE_PATH = "ParallaxSettings";
+
+		[HideInInspector]
+		[SerializeField]
+		private List<Vector2Variable> _vector2Variables = new List<Vector2Variable>();
+
+		public List<Vector2Variable> Vector2Variables {
+			get { return _vector2Variables; }
+		}
+
+		public static ParallaxSettings Current {
+			get {
+				if (_current == null) {
+					_current = Resources.Load<ParallaxSettings>(RESOURCE_PATH);
+					Debug.AssertFormat(
+						_current != null,
+						"Could not load {1}. Please verify a {1} object is at `Resources/{0}'. If not please create one.",
+						RESOURCE_PATH,
+						typeof(ParallaxSettings).FullName);
+				}
+
+				return _current;
+			}
+		}
+	}
+}
