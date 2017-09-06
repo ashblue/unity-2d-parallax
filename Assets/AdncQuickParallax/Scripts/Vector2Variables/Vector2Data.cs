1e47cfee92503759673695700d34a9f23a41993d
Ash Blue <ash@blueashes.com>
2017-09-06 11:57:32 -0600

    New custom drawers for Vector2 variables

---
+++ Assets/AdncQuickParallax/Scripts/Vector2Variables/Vector2Data.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Adnc.QuickParallax {
+	[System.Serializable]
+	public class Vector2Data {
+		[SerializeField]
+		private Vector2 _value;
+
+		[SerializeField]
+		private Vector2Variable _variable;
+
+		public Vector2 Value {
+			get {
+				if (_variable == null) {
+					return _value;
+				}
+
+				return _variable.Value;
+			}
+
+			set { _value = value; }
+		}
+	}
+}
+
