ac94b6098acdf3935617fec3be638ac1e01170f6
Ash Blue <ash@blueashes.com>
2017-09-11 17:33:15 -0600

    Added parallax groups with the ability to restore all nested layer positions

---
+++ Assets/AdncQuickParallax/Scripts/Layers/Groups/ParallaxLayerGroup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Adnc.QuickParallax {
+	public class ParallaxLayerGroup : MonoBehaviour {
+		[SerializeField]
+		private ParallaxLayer[] _layers;
+
+		private void Awake () {
+			if (_layers == null || _layers.Length == 0) {
+				_layers = GetChildLayers();
+			}
+		}
+
+		ParallaxLayer[] GetChildLayers () {
+			return GetComponentsInChildren<ParallaxLayer>(true);
+		}
+
+		public void RestoreOriginPositions () {
+			foreach (var parallaxLayer in _layers) {
+				if (parallaxLayer == null) continue;
+				parallaxLayer.RestoreOriginPosition();
+			}
+		}
+	}
+}
+
