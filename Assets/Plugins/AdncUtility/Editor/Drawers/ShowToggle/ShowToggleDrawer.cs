47227713b693740c5d3d91388416fbeccaa28c58
Ash Blue <ash@blueashes.com>
2017-09-13 16:59:49 -0600

    Upgraded utilities package

--- Assets/Plugins/AdncUtility/Editor/Drawers/ShowToggle/ShowToggleDrawer.cs
+++ Assets/Plugins/AdncUtility/Editor/Drawers/ShowToggle/ShowToggleDrawer.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -8,40 +8,75 @@ namespace Adnc.Utility.Drawers {
     public class ShowToggleDrawer : PropertyDrawer {
         public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
             var toggle = (ShowToggleAttribute)attribute;
-            var isVisible = GetIsVisible(toggle, property);
+            var display = GetDisplay(toggle, property);
 
-            if (isVisible) {
-                EditorGUI.PropertyField(position, property, label, true);
-                return;
-            }
-
-            if (toggle.invalidDisplay == ShowToggleDisplay.Hide) return;
-
-            if (toggle.invalidDisplay == ShowToggleDisplay.Disable) {
-                GUI.enabled = false;
-                EditorGUI.PropertyField(position, property, label, true);
-                GUI.enabled = true;
+            switch (display) {
+                case ShowToggleDisplay.Show:
+                    EditorGUI.PropertyField(position, property, label, true);
+                    break;
+                case ShowToggleDisplay.Hide:
+                    break;
+                case ShowToggleDisplay.Disable:
+                    GUI.enabled = false;
+                    EditorGUI.PropertyField(position, property, label, true);
+                    GUI.enabled = true;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
 
         public override float GetPropertyHeight (SerializedProperty property, GUIContent label) {
             var toggle = (ShowToggleAttribute)attribute;
-            var isVisible = GetIsVisible(toggle, property);
+            var display = GetDisplay(toggle, property);
 
-            if (isVisible || toggle.invalidDisplay == ShowToggleDisplay.Disable) {
-                return base.GetPropertyHeight(property, label);
+            switch (display) {
+                case ShowToggleDisplay.Show:
+                    return EditorGUI.GetPropertyHeight(property, label);
+                case ShowToggleDisplay.Hide:
+                    return 0;
+                case ShowToggleDisplay.Disable:
+                    return EditorGUI.GetPropertyHeight(property, label);
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
-
-            return 0;
         }
 
-        bool GetIsVisible (ShowToggleAttribute toggle, SerializedProperty property) {
+        static ShowToggleDisplay GetDisplay (ShowToggleAttribute toggle, SerializedProperty property) {
+            var isValid = GetIsValid(toggle, property);
+
+            return isValid ? toggle.validDisplay : toggle.invalidDisplay;
+        }
+
+        static bool GetIsValid (ShowToggleAttribute toggle, SerializedProperty property) {
+            if (toggle.IsEnum) {
+                return GetIsValidEnum(toggle, property);
+            }
+
+            return GetIsValidBool(toggle, property);
+        }
+
+        static bool GetIsValidBool (ShowToggleAttribute toggle, SerializedProperty property) {
             var propertyPath = property.propertyPath;
             var conditionPath = propertyPath.Replace(property.name, toggle.fieldName);
             var condition = property.serializedObject.FindProperty(conditionPath);
 
             if (condition != null) {
                 return condition.boolValue == toggle.requiredValue;
+            }
+
+            Debug.LogWarningFormat("[ShowToggle] could not find attribute {0}", toggle.fieldName);
+
+            return true;
+        }
+
+        static bool GetIsValidEnum (ShowToggleAttribute toggle, SerializedProperty property) {
+            var propertyPath = property.propertyPath;
+            var conditionPath = propertyPath.Replace(property.name, toggle.fieldName);
+            var condition = property.serializedObject.FindProperty(conditionPath);
+
+            if (condition != null) {
+                return toggle.requiredEnumValue.Contains(condition.enumValueIndex);
             }
 
             Debug.LogWarningFormat("[ShowToggle] could not find attribute {0}", toggle.fieldName);
--- Assets/Plugins/AdncUtility/Scripts/Attributes/ShowToggle/ShowToggleAttribute.cs
+++ Assets/Plugins/AdncUtility/Scripts/Attributes/ShowToggle/ShowToggleAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace Adnc.Utility {
     public class ShowToggleAttribute : PropertyAttribute {
+        private const ShowToggleDisplay DEFAULT_VALID_DISPLAY = ShowToggleDisplay.Show;
+        private const ShowToggleDisplay DEFAULT_INVALID_DISPLAY = ShowToggleDisplay.Hide;
+
         /// <summary>
         /// Name of the field required to show this attribute. Accepts dot notation.
         /// Example `myObj.myProp`. Must point to a field that is a boolean.
@@ -14,14 +17,40 @@ namespace Adnc.Utility {
         public bool requiredValue;
 
         /// <summary>
+        /// Value required by the fieldName's value when turned into an enum
+        /// </summary>
+        public int[] requiredEnumValue;
+
+        /// <summary>
         /// How to handle displaying an invalid value
         /// </summary>
         public ShowToggleDisplay invalidDisplay;
 
-        public ShowToggleAttribute (string fieldName, bool requiredValue = true, ShowToggleDisplay invalidDisplay = ShowToggleDisplay.Hide) {
+        /// <summary>
+        /// How to handle displaying a valid value
+        /// </summary>
+        public ShowToggleDisplay validDisplay;
+
+        public bool IsEnum {
+            get { return requiredEnumValue != null; }
+        }
+
+        public ShowToggleAttribute (
+            string fieldName, bool requiredValue = true, ShowToggleDisplay invalidDisplay = DEFAULT_INVALID_DISPLAY,
+            ShowToggleDisplay validDisplay = DEFAULT_VALID_DISPLAY) {
             this.fieldName = fieldName;
             this.requiredValue = requiredValue;
             this.invalidDisplay = invalidDisplay;
+            this.validDisplay = validDisplay;
+        }
+
+        public ShowToggleAttribute (
+            string fieldName, int[] requiredEnumValue, ShowToggleDisplay invalidDisplay = DEFAULT_INVALID_DISPLAY,
+            ShowToggleDisplay validDisplay = DEFAULT_VALID_DISPLAY) {
+            this.fieldName = fieldName;
+            this.requiredEnumValue = requiredEnumValue;
+            this.invalidDisplay = invalidDisplay;
+            this.validDisplay = validDisplay;
         }
     }
 }
